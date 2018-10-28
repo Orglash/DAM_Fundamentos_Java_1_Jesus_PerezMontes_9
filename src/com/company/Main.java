98e3d20756de6635742f58973cdd5d12a203cb12
Orglash <jesus.perez@escuelaestech.es>
2018-10-28 16:15:23 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Pedimos los dos números por consola y los asignamos a sus variables.
+        System.out.println("Introduzca el primer número \"A\":\n");
+        Scanner sc=new Scanner(System.in);
+        int numeroA=sc.nextInt();
+        System.out.println("Introduzca el segundo número \"B\":\n");
+        int numeroB=sc.nextInt();
+        System.out.println("Introduzca el tercer número \"C\":\n");
+        int numeroC=sc.nextInt();
+
+        //Comparamos ambos números y en función de cual es el mayor, los mostramos en orden.
+
+        if (numeroA>=numeroB) {
+            if(numeroA>=numeroC){
+                if(numeroB>=numeroC){
+                    System.out.printf("%d, %d, %d.\n", numeroA, numeroB, numeroC);
+                }else {
+                    System.out.printf("%d, %d, %d.\n", numeroA, numeroC, numeroB);
+                }
+            }else{
+                System.out.printf("%d, %d, %d.\n", numeroC, numeroA, numeroB);
+            }
+        } else {
+            if(numeroB>=numeroC){
+                if(numeroA>=numeroC){
+                    System.out.printf("%d, %d, %d.\n", numeroB, numeroA, numeroC);
+                }else{
+                    System.out.printf("%d, %d, %d.\n", numeroB, numeroC, numeroA);
+                }
+            }else{
+                System.out.printf("%d, %d, %d.\n", numeroC, numeroB, numeroA);
+            }
+        }
+    }
+}
